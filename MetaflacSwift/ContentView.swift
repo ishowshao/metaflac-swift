1066238d06a6c08a66e44b2e8db9422bb41d546b
zimu <234047485@qq.com>
2024-10-29 23:49:05 +0800

    init

---
+++ MetaflacSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetaflacSwift
+//
+//  Created by 荒野老男人 on 2024/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetaflacSwift/MetaflacSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaflacSwiftApp.swift
+//  MetaflacSwift
+//
+//  Created by 荒野老男人 on 2024/10/29.
+//
+
+import SwiftUI
+
+@main
+struct MetaflacSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
